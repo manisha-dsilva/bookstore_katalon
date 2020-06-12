efbc68203e0ba8cdd7b573000ad690281022d15f
Mamatha <mamatha.k@al-enterprise.com>
2020-06-12 15:23:55 +0530

    Folder added

--- run_chrome.sh
+++ run_chrome.sh
@@ -1,4 +1,5 @@
-rm -rfv ./reports/chrome
-mkdir -p ./reports/chrome
+#!/usr/bin/env bash
 
-docker run -t --rm -v $(pwd):/katalon/katalon/source katalonstudio/katalon:6.3.3 katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/BookStore"  -execution Profile="default"
+set -xe
+
+docker run -t --rm -v "$(pwd)":/katalon/katalon/source katalonstudio/katalon katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/BookStore"
