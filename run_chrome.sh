4620fe69891872c9349e075963343cc52db5f87f
Mamatha <mamatha.k@al-enterprise.com>
2020-06-12 13:59:43 +0530

    Modified

--- run_chrome.sh
+++ run_chrome.sh
@@ -1,4 +1,4 @@
 rm -rfv ./reports/chrome
 mkdir -p ./reports/chrome
 
-docker run -t --rm -v $(pwd):/katalon/katalon/source katalonstudio/katalon:6.3.3 katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/BookStore"  -execution Profile="default" -projectPath=/tmp/katalon_execute/project/bookstore_automation.prj
+docker run -t --rm -v $(pwd):/katalon/katalon/source katalonstudio/katalon:6.3.3 katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/BookStore"  -execution Profile="default" -projectPath=/tmp/katalon_execute/bookstore_automation.prj
