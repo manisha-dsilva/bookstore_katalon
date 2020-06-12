888aa53a293d64904dbe28d6e7a0ad2a3d413f44
Mamatha <mamatha.k@al-enterprise.com>
2020-06-12 12:27:18 +0530

    Added run.sh file for chrome

--- run_chrome.sh
+++ run_chrome.sh
@@ -1,4 +1,4 @@
 rm -rfv ./reports/chrome
 mkdir -p ./reports/chrome
 
-docker run -t --rm -v $(pwd):/katalon/katalon/source katalonstudio/katalon:6.3.3 katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/BookStore"  -execution Profile="default" -projectPath=$(pwd)
+docker run -t --rm -v $(pwd):/katalon/katalon/source katalonstudio/katalon:6.3.3 katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/BookStore"  -execution Profile="default" -projectPath=/katalon/katalon/source
