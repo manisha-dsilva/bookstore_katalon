f04511426d470fe18b847d9c981562ef100a05bb
Dsilva Manisha Audrey <manisha.dsilva@al-enterprise.com>
2020-07-28 11:56:04 +0530

    test

--- Libs/internal/GlobalVariable.groovy
+++ Libs/internal/GlobalVariable.groovy
@@ -1,12 +1,8 @@
 package internal
 
 import com.kms.katalon.core.configuration.RunConfiguration
-import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
-import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
-import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import com.kms.katalon.core.main.TestCaseMain
+
 
 /**
  * This class is generated automatically by Katalon Studio and should not be modified or deleted.
@@ -25,15 +21,16 @@ public class GlobalVariable {
      
 
     static {
-        def allVariables = [:]        
-        allVariables.put('default', ['url' : 'http://135.254.163.44:4200', 'test' : 'test'])
-        allVariables.put('New Profile', allVariables['default'] + [:])
-        
-        String profileName = RunConfiguration.getExecutionProfile()
-        
-        def selectedVariables = allVariables[profileName]
-        url = selectedVariables['url']
-        test = selectedVariables['test']
-        
+        try {
+            def selectedVariables = TestCaseMain.getGlobalVariables("default")
+			selectedVariables += TestCaseMain.getGlobalVariables(RunConfiguration.getExecutionProfile())
+            selectedVariables += RunConfiguration.getOverridingParameters()
+    
+            url = selectedVariables['url']
+            test = selectedVariables['test']
+            
+        } catch (Exception e) {
+            TestCaseMain.logGlobalVariableError(e)
+        }
     }
 }
