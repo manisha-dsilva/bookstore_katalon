1f474a9a3bc23bd1a47e7d78cc89a6188538d978
Dsilva Manisha Audrey <manisha.dsilva@al-enterprise.com>
2020-08-17 20:27:12 +0530

    pipeline

---
+++ Test Suites/1.1 BookStore.ts
@@ -0,0 +1,98 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>1.1 BookStore</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a020c18c-ca47-49d3-8069-bf3e8092101d</testSuiteGuid>
+   <testCaseLink>
+      <guid>0ec34f1b-58ae-4bec-a9f7-558a08683f8a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AddDevopsDoc</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>9fa52aed-fde1-4c75-8432-ca054a30be6c</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>0ac99007-8498-4c6f-b96c-84d4793a6cd7</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>915d8fa5-62dc-44bc-a4de-c0efc6c27373</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AddMavenBook</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>23568845-02da-4cad-b677-5c9754b721e6</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>a29b8bad-2f4b-4c01-9770-74fcfe3cde0e</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>0f18dde5-5898-4eb0-a94f-7bd02457fa0e</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b4645666-e4dc-46d8-b488-2d496897d85e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AllBooks</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>6f575b8b-10eb-417d-8b9f-d41b031092b4</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>515dc378-8172-40b4-82bf-f77c6138ba29</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AllDocs</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>741d129f-1520-454f-88ef-0574fba143a7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AWSDocs</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cf563217-d397-442a-af24-571138f5835c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DevopsDoc</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>49f9ccd1-89e6-41d7-bfc7-3cf119c1f4e1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JenkinsBook</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>58d493a2-7e50-4c23-9a6b-0f6e9a494a18</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LinuxBook</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
